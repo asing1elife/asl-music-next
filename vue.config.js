04cf31297eb109dc8d9332b8763779639b6953d4
asing1elife <asing1elife@139.com>
2021-07-03 15:21:27 +0800

    feat: 2-3 基础代码编写

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  css: {
+    loaderOptions: {
+      sass: {
+        // 全局引入样式文件，该语句在项目编译时会被动态插入到每个 vue 文件 <style> 节点中
+        // 需要 node-sass 5.x 以及 sass-loader 10.x
+        additionalData: `
+          @import "@/assets/scss/variable.scss";
+          @import "@/assets/scss/mixin.scss";
+        `
+      }
+    }
+  }
+}
